dd123239a8f6bde3b1114e0c9e73b56e6cb155b2
Eduard Castro <eduard.cas07@gmail.com>
2019-11-27 22:35:33 -0500

    Initial commit

---
+++ src/test/java/com/computerly/conferencespringboot/ConferenceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computerly.conferencespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
